083f3e53a47f2a07f70dd9c485577ec37a33c038
flover-luffy <flovertoluffy@gmail.com>
2025-07-04 13:50:41 +0800

    修改依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    val kotlinVersion = "1.7.10"
+    val kotlinVersion = "2.0.21"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.14.0"
+    id("net.mamoe.mirai-console") version "2.16.0"
 }
 
 group = "net.lawaxi"
@@ -11,10 +11,11 @@ version = "0.1.1-test7"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public")
+    maven("https://mcl.repo.mamoe.net/")
     mavenCentral()
 }
 
 dependencies {
-    api ("cn.hutool:hutool-all:5.8.18")
+    api ("cn.hutool:hutool-all:5.8.38")
     api(files("libs/shitboy-0.1.9.mirai2.jar"))
 }
