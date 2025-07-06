20cf2bc906c67075aa7c7890640e3540586dad95
flover-luffy <96760096+flover-luffy@users.noreply.github.com>
2025-07-06 17:10:47 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     val kotlinVersion = "2.0.21"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-
     id("net.mamoe.mirai-console") version "2.16.0"
 }
 
@@ -16,6 +15,11 @@ repositories {
 }
 
 dependencies {
-    api ("cn.hutool:hutool-all:5.8.38")
+    api("cn.hutool:hutool-all:5.8.38")
     api(files("libs/shitboy-0.1.11-dev7.jar"))
+    
+    // 新增依赖
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("org.slf4j:slf4j-api:1.7.32")
+    implementation("ch.qos.logback:logback-classic:1.2.6")
 }
